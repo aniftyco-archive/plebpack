cea7a2405bbd0013547254cfcd2c92fa18703956
Josh Manders <josh@joshmanders.com>
2019-03-28 20:54:46 -0500

    export from hooks instead of index

--- packages/plebpack/hooks.js
+++ packages/plebpack/hooks.js
@@ -1,1 +1,13 @@
-module.exports = require('./lib/hooks');
+const {context} = require('./lib/hooks/context');
+const {entry} = require('./lib/hooks/entry');
+const {loader} = require('./lib/hooks/loader');
+const {output} = require('./lib/hooks/output');
+const {plugin} = require('./lib/hooks/plugin');
+
+module.exports = {
+  context,
+  entry,
+  loader,
+  output,
+  plugin,
+};
--- packages/plebpack/src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import context from './context';
-import entry from './entry';
-import loader from './loader';
-import output from './output';
-import plugin from './plugin';
-
-export {context, entry, output, loader, plugin};
