69874aa631e7f92c6ddf1b4293711df7fe6f40bd
Josh Manders <josh@joshmanders.com>
2019-03-28 23:07:54 -0500

    simplify even more

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,7 @@
-const {resolve} = require('path');
 const config = require('plebpack');
-const {context, entry, output} = require('plebpack/hooks');
+const {entry, output} = require('plebpack/hooks');
 
-config.use(context(resolve(__dirname, 'scripts')));
-config.use(entry('./index.js'));
+config.use(entry('./scripts/index.js'));
 config.use(output('./build/[name].js'));
 
 module.exports = config;
