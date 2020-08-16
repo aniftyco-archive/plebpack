cf6e421fb9709a167340d2ed113187bcadbc53a8
Josh Manders <josh@joshmanders.com>
2020-08-16 15:45:38 -0500

    move jest, eslint and prettier config into package.json

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: 'nifty',
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-  },
-};
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  printWidth: 80,
-  singleQuote: true,
-  trailingComma: 'es5',
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-};
